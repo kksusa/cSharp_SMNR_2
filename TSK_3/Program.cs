bc308758eebe94fa870c61d76c6aa19768dfb687
kksusa <kksusa1@gmail.com>
2022-08-13 18:47:18 +0500

    задание 3

--- TSK_3/Program.cs
+++ TSK_3/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.WriteLine("Напишите целое число от 1 до 7:");
+string? numberstring = Console.ReadLine();
+int number = int.Parse(numberstring!);
+if (number > 7 || number < 1) Console.WriteLine("Вы неправильно ввели число.");
+else if (number >= 1 && number <= 5) Console.WriteLine("День не является выходным.");
+else Console.WriteLine("Ура! Выходной!");
