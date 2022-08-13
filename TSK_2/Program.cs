56ffc86f41dc2987e5059496fbbdb38332f8df48
kksusa <kksusa1@gmail.com>
2022-08-13 18:46:57 +0500

    задание 2

--- TSK_2/Program.cs
+++ TSK_2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Напишите целое число:");
+string? numberstring = Console.ReadLine();
+int number = int.Parse(numberstring!);
+int thirdDigit = 0;
+int count = 100;
+if (number / 100 == 0) Console.WriteLine("Третьей цифры нет.");
+else
+{
+    while (number / count != 0) count *= 10;
+    thirdDigit = number / (count / 1000) % 10;
+    if (thirdDigit < 0) thirdDigit *= -1;
+    Console.WriteLine("Третья цифра этого числа " + thirdDigit + ".");    
+}
